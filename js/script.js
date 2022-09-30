7cab1eff4d22514d77adbf1bd1ef6cb3b5d26a39
Yerson18 <yceroncarvajal@gmail.com>
2022-09-30 10:57:01 -0500

    nuevaactualizacion

--- js/script.js
+++ js/script.js
@@ -1,1 +1,14 @@
-let barMenu
+let barMenu = document.getElementById("barMenu");
+let menuUl = document.getElementById("menuUL");
+
+barMenu.addEventListener("click", function() {
+    if (menuUl.classList.contains("menu-horiz")) {
+        menuUl.classList.remove("menu-horiz");
+        menuUl.classList.add("menu-responsive");
+    }
+    else {
+        menuUl.classList.add("menu-horiz");
+        menuUl.classList.remove("menu-responsive");
+    }
+})
+     
